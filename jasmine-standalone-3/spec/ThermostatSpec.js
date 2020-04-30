3cfae23103d1f6d3f47573df39466f0ef8d4a96e
Bart Judge <bartjudge@Barts-MacBook-Pro.local>
2020-04-30 20:46:27 +0100

    can increase the temperature

--- jasmine-standalone-3/spec/ThermostatSpec.js
+++ jasmine-standalone-3/spec/ThermostatSpec.js
@@ -1,7 +1,13 @@
 describe('Thermostat', function() {
   var thermostat;
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  })
   it('starts with a temperature of 20 degrees', function() {
-    var thermostat = new Thermostat();
-    expect(thermostat._currentTemperature).toEqual(20)
+    expect(thermostat._currentTemperature).toEqual(20);
+  });
+  it('can increase the temperature', function() {
+    thermostat.increase(1);
+    expect(thermostat._currentTemperature).toEqual(21);
   });
 });
--- jasmine-standalone-3/src/Thermostat.js
+++ jasmine-standalone-3/src/Thermostat.js
@@ -1,3 +1,7 @@
 var Thermostat = function() {
    this._currentTemperature = 20
 };
+
+Thermostat.prototype.increase = function(number) {
+  this._currentTemperature += number;
+}
